334cb0ad4109f016f89d096c2261b0d2bc2003a8
isuretpolos <isuret.polos@gmail.com>
2026-07-31 13:39:03 +0200

    Cleaning a bit up

---
+++ SCRIPTS/ESP32/esp32trng.c
@@ -0,0 +1,18 @@
+#include "esp_system.h"
+#include "esp_random.h"
+#include "bootloader_random.h"
+
+void setup() {
+  Serial.begin(921600);
+  delay(1000);
+  // Activate entropy
+  bootloader_random_enable();
+}
+
+void loop() {
+  uint8_t buffer[256];
+
+  esp_fill_random(buffer, sizeof(buffer));
+
+  Serial.write(buffer, sizeof(buffer));
+}
